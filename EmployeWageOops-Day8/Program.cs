e8bc31544c6af0e3ea5319ddd37d5b1c3bf00ba4
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-17 13:42:23 +0530

    Welcome to Employee Wage Computation Program

---
+++ EmployeWageOops-Day8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeWageOops_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employe Wage Computation");
+        }
+    }
+}
