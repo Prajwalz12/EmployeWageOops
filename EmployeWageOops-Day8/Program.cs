9645280c3d157317873381adba216fd9b15a31ff
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-17 13:44:26 +0530

    adding employee is present or absent program

--- EmployeWageOops-Day8/Program.cs
+++ EmployeWageOops-Day8/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace EmployeWageOops_Day8
+namespace EmployeeWage
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employe Wage Computation");
+            int rand = new Random().Next(0, 2);
+            if (rand == 0)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+
+            }
         }
     }
 }
